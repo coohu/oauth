da94601b5140ee9545bf92355471bba5f0284fff
coohu <1257817341@qq.com>
2026-01-09 18:41:17 +0800

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use crate::{config::Config, router::build_router, state::AppState};
-use tokio::{signal, net::TcpListener};
+use tokio::{signal};
 use anyhow::Result;
 use tracing::info;
 
@@ -16,19 +16,19 @@ mod util;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let config = Config::from_env();
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::from_default_env())
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let config = Config::from_env();
     let db = db::Database::connect(&config.database_url).await?;
     db.init().await?;
 
     let state = AppState { config, db };
     let app = build_router(state.clone());
 
-    let listener = TcpListener::bind(state.config.bind).await?;
+    let listener = tokio::net::TcpListener::bind(state.config.bind).await?;
     info!("Listening on {}", state.config.bind);
 
     axum::serve(listener, app)
